fc9812d4dedb3546b2d2eaf2b38d784953bfb0d8
Timur Golubovich <timur.golubovich@syntacore.com>
2024-10-04 21:02:32 +0300

    added omp tasks 1

--- OMP_Tasks/balance.cpp
+++ OMP_Tasks/balance.cpp
@@ -1,35 +1,56 @@
-#include <stdio.h>
+#include <cassert>
+#include <cstdlib>
+#include <iostream>
 #include <omp.h>
+#include <stdio.h>
 
-//#define SCHEDULE_STATIC
-//#define SCHEDULE_DYNAMIC
-//#define SCHEDULE_GUIDED
-//#define SCHEDULE_DEFAULT
+enum Mode {
+  STATIC = 1,
+  DYNAMIC = 2,
+  GUIDED = 3,
+};
 
-#ifdef SCHEDULE_STATIC
-    #define PARALLEL_STATEMENT schedule(static, scheduleChunkSize)
-#elif defined(SCHEDULE_DYNAMIC)
-    #define PARALLEL_STATEMENT schedule(dynamic, scheduleChunkSize)
-#elif defined(SCHEDULE_GUIDED)
-    #define PARALLEL_STATEMENT schedule(guided, scheduleChunkSize)
-#elif defined(SCHEDULE_DEFAULT)
-    #define PARALLEL_STATEMENT
-#endif
+long long int fact(long long int n) { return n <= 1 ? 1 : n * fact(n - 1); }
 
-int main()
-{
+auto main(int argc, const char *argv[]) -> int {
   const int itersCount = 65;
   const int threadsCount = 4;
   const int scheduleChunkSize = 4;
-
+  if (argc != 2) {
+    std::cout << "usage: mode (1 - static, 2 - dynamic, 3 - guided)"
+              << std::endl;
+    return -1;
+  }
+  const int modeType = std::atoi(argv[1]);
   omp_set_dynamic(0);
   omp_set_num_threads(threadsCount);
 
   printf("Thread Iteration\n");
+  switch (modeType) {
+  case Mode::STATIC: {
+#pragma omp parallel for schedule(static, scheduleChunkSize)
+    for (int i = 0; i < itersCount; i++) {
+      printf("%d %d %lld\n", omp_get_thread_num(), i, fact(i));
+    }
+    break;
+  }
+  case Mode::DYNAMIC: {
+#pragma omp parallel for schedule(dynamic, scheduleChunkSize)
+    for (int i = 0; i < itersCount; i++) {
+      printf("%d %d %lld\n", omp_get_thread_num(), i, fact(i));
+    }
+    break;
+  }
+  case Mode::GUIDED: {
 
-  #pragma omp parallel for PARALLEL_STATEMENT
-  for(int i = 0; i < itersCount; i++) {
-      printf("%d %d\n", omp_get_thread_num(), i);
+#pragma omp parallel for schedule(guided, scheduleChunkSize)
+    for (int i = 0; i < itersCount; i++) {
+      printf("%d %d %lld\n", omp_get_thread_num(), i, fact(i));
+    }
+    break;
+  }
+  default:
+    assert(0 && "Unreachable");
   }
 
   return 0;
