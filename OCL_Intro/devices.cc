425f5bf566c8693d385ee0f3c4b937a99aedfd9f
kefirRzevo <golubovich.ts@phystech.edu>
2024-12-06 16:05:20 +0300

    added ocl defines

--- OCL_Intro/devices.cc
+++ OCL_Intro/devices.cc
@@ -1,3 +1,10 @@
+#ifndef CL_HPP_TARGET_OPENCL_VERSION
+#define CL_HPP_MINIMUM_OPENCL_VERSION 120
+#define CL_HPP_TARGET_OPENCL_VERSION 120
+#endif                                                
+#define CL_HPP_CL_1_2_DEFAULT_BUILD
+#define CL_HPP_ENABLE_EXCEPTIONS
+
 #ifdef __APPLE__
 #include "OpenCL/opencl.hpp"
 #else
